3ebbe0a94ef1568a5c67a5b8d5ae3d496d1ee065
unknown <arunmohan471@gmail.com>
2017-01-12 10:27:51 +0530

    updated upto footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-function showModal(){
-  $(".ui.modal").modal('show');
+setInterval(shapeQuotes, 2000);
+setInterval(shapeFacts, 3000);
+setInterval(shapeSpells, 5000);
+
+function showModal() {
+    $(".ui.modal").modal('show');
 }
+
+function shapeQuotes() {
+    $('#quotes').shape('flip up');
+}
+
+function shapeFacts() {
+    $('#facts').shape('flip up');
+}
+
+function shapeSpells() {
+    $('#spells').shape('flip down');
+}
+
+$(document).ready(function(){
+  $('.right.menu.open').on("click",function(e){
+    e.preventDefault();
+    $('.ui.vertical.menu').toggle();
+  });
+
+  $('.ui.dropdown').dropdown();
+});
